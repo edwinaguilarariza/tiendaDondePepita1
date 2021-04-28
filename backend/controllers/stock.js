8a8fefad1bdf328018fd7bcb9a169e2cce63584f
edwinaguilarariza <edwinaguilarariza1@yahoo.com>
2021-04-28 17:51:16 -0500

    coreccion en idProducto stock

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -9,32 +9,32 @@ const registrarStock = (req, res) => {
     //registramos producto osea curso
     //validamos los campos antes de registrar
     if (
-      params.nombre &&
-      params.descripcion &&
-      params.cantidad
-    ) {
-      //lleamos el modelo con lls datos del req
-      stock.nombre = params.nombre;
-      stock.descripcion = params.descripcion;
-      stock.cantidad = params.cantidad;
+      params.cantidad &&
+      params.idProducto
+      ) {
+        stock.cantidad = params.cantidad;
+        stock.idProducto = params.idProducto;
+        stock.save((err, datosStock) => {
+          if (err) {
+            //porque solo es true
+            res.status(500).send({ mensaje: "error al conectar al servidor" });
+          } else {
+            if (datosStock) {
+              res.status(200).send({ stock: datosStock });
+            } else {
+              res.status(401).send({ mensaje: "no se puedo registrar el stock" });
+            }
+          }
+        });
+      } else {
+        res.status(401).send({ mensaje: "falto algunos de los datos" });
+      }
+    };
+      
+      
+      
       
       //registramos el curso
-      stock.save((err, datosStock) => {
-        if (err) {
-          //porque solo es true
-          res.status(500).send({ mensaje: "error al conectar al servidor" });
-        } else {
-          if (datosStock) {
-            res.status(200).send({ stock: datosStock });
-          } else {
-            res.status(401).send({ mensaje: "no se puedo registrar el stock" });
-          }
-        }
-      });
-    } else {
-      res.status(401).send({ mensaje: "falto algunos de los datos" });
-    }
-  };
   
 module.exports = {
     registrarStock, 
--- backend/modelo/stock.js
+++ backend/modelo/stock.js
@@ -4,13 +4,9 @@ let Schema = mongoose.Schema;
 
 // Se crea el esquema
 let stockSchema = Schema({
-  nombre: String,
-  descripcion: String,
-  cantidad: Number,
-  
-  
+  cantidad: String,
   idProducto: { type: Schema.ObjectId, ref: "producto" },
-  
 });
+
 // Se exporta el modulo
 module.exports = mongoose.model("stock", stockSchema);
